542e7a35fea5832d1f141c6347ea4aebd76e53d5
Sergey <pewpewinmyhead@gmail.com>
2021-09-08 12:17:21 +0300

    adaptive (<1400 <1200 <991)

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,20 @@
 'use strict';
 
+window.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('.header'),
+        burger = document.querySelector('.burger');
+
+  burger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+  })
+  
+});
+
+const first = [1, 2, 3, 4];
+const second = [3, 4, 5, 6];
+
+function intersection (a, b) {
+  const filteredArray = a.filter(value => b.includes(value));
+  console.log(filteredArray);
+}
+intersection(first, second);
