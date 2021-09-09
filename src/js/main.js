69683ccf061280cf4174634a15e00ff33479d6f3
Sergey <pewpewinmyhead@gmail.com>
2021-09-09 14:50:47 +0300

    mobile adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,11 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-  const menu = document.querySelector('.header'),
+  const menu = document.querySelector('.menu__list'),
         burger = document.querySelector('.burger');
 
   burger.addEventListener('click', () => {
-    menu.classList.toggle('active');
+    menu.classList.toggle('menu__list_active');
   })
   
 });
-
-const first = [1, 2, 3, 4];
-const second = [3, 4, 5, 6];
-
-function intersection (a, b) {
-  const filteredArray = a.filter(value => b.includes(value));
-  console.log(filteredArray);
-}
-intersection(first, second);
