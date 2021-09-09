9d9a997063a1d3de341b9a97778b53857c9fd002
Sergey <pewpewinmyhead@gmail.com>
2021-09-09 19:47:22 +0300

    add hiding mobile menu to click on item

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,17 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   const menu = document.querySelector('.menu__list'),
+        menuItem = document.querySelectorAll('.menu__list-item'),
         burger = document.querySelector('.burger');
 
   burger.addEventListener('click', () => {
     menu.classList.toggle('menu__list_active');
-  })
+  });
+
+  menuItem.forEach(item => {
+    item.addEventListener('click', () => {
+      menu.classList.toggle('menu__list_active');
+    });
+  });
   
 });
